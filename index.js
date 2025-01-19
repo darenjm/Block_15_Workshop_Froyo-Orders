5f19a32cf4765970b95920d61a22c2f8dd85445b
Daren Morreale <daren@givelegacy.com>
2025-01-19 13:10:41 -0500

    updated code

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 //create prompt
 const userInput = prompt("Enter Froyo flavors separated by commas:");
 
-// console.log(userInput);
-//userInput --> vanilla,vanilla,vanilla,strawberry,coffee,coffee
-
 //spilt userInput using ,
 const order = userInput.split(",");
 
@@ -22,16 +19,16 @@ function orderQty(order) {
       obj[flavor] = 1;
     }
   }
+
   //add the flavor as a key, with a value of 1 if the key doesn't exist otherwise add 1 to the current value
   return obj;
   //ways to add 1 to current value obj[key] = obj[key]++ || obj[key]+=1
-
+  // incrementOrderQty(order);
   // console.table(obj);
   //return obj created above
   // return -1;
 }
 
 // console.log(orderQty(order));
-// console.log(orderQty([1, 2, 3]));
 
 console.table(orderQty(order));
