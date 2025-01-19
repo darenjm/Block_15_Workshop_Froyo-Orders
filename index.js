e3889d180d0b9114cc3d3f4439e30e513995753a
Daren Morreale <daren@givelegacy.com>
2025-01-19 12:07:08 -0500

    struggling with the solution

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 //create prompt
 const userInput = prompt("Enter Froyo flavors separated by commas:");
 
-console.log(userInput);
+// console.log(userInput);
 //userInput --> vanilla,vanilla,vanilla,strawberry,coffee,coffee
 
 //spilt userInput using ,
 const order = userInput.split(",");
 
+console.log(order);
+
 //create function
-function orderQty(computer) {
+function orderQty(order) {
   //create empty obj
-  const purple = "hello world!!";
-  console.log(purple, computer);
   const obj = {};
   //loop over array
+  for (let i = 0; i < order.length; i++) {
+    const flavor = order[i];
+    if (obj[flavor]) {
+      obj[flavor]++;
+    } else {
+      obj[flavor] = 1;
+    }
+  }
   //add the flavor as a key, with a value of 1 if the key doesn't exist otherwise add 1 to the current value
+  return obj;
   //ways to add 1 to current value obj[key] = obj[key]++ || obj[key]+=1
 
   // console.table(obj);
   //return obj created above
-  return -1;
+  // return -1;
 }
 
-console.log(orderQty(order));
-console.log(orderQty([1, 2, 3]));
+// console.log(orderQty(order));
+// console.log(orderQty([1, 2, 3]));
+
+console.table(orderQty(order));
